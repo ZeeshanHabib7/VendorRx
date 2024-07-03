bde4046a07dec26a28185f717cc47d000d77c3af
Syed Samroze Ali <samroze3@gmail.com>
2024-07-03 05:16:27 -0700

    Task Complete

---
+++ app/Helpers/ResponseHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+// app/Helpers/DateHelper.php
+
+namespace App\Helpers;
+
+
+class ResponseHelper
+{
+    public static function sendResponse($sucess, $code, $msg, $products)
+    {
+        $response = [
+            'success' => $sucess,
+            'status_code' => $code,
+            'message' => [$msg],
+            'data' => [
+                'list' => $products
+            ]
+
+        ];
+
+        return $response;
+    }
+}
