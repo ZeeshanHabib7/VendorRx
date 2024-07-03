5a511ac3fb48950c354f280bc50273320e7a8580
hfatima08 <fatimaah812@gmail.com>
2024-07-03 22:47:11 +0500

    added accessors to format columns 'created_at & updated_at'

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Product extends Model
 {
     use HasFactory;
+
     protected $fillable = ['name', 'price', 'date', 'brand'];
+
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+    }
+
+    public function getUpdatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['updated_at'])->format('Y-m-d H:i:s');
+    }
 }
