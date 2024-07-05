8c59c8a377b4400742b7f6b57c4c87b29700210b
hfatima08 <fatimaah812@gmail.com>
2024-07-05 17:03:03 +0500

    updated pagination code

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -1,28 +1,22 @@
 <?php
 
 namespace App\Http\Resources;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\ResourceCollection;
-
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
      *
      * @return array<int|string, mixed>
      */
-    public function toArray(Request $request): array
-    {
-        return [
-            'data' => $this->collection->transform(function($product) {
-                return [
-                    'id' => $product->id,
-                    'name' => $product->name,
-                    'brand' => $product->brand,
-                    'price' => $product->price,
-                ];
-            }),
+    public function toArray($request) { 
+        return [ 
+            'id' => $this->id,
+            'name'=> $this->name,
+            'price'=> $this->price,
+            'brand'=> $this->brand
         ];
     }
+    
 }
