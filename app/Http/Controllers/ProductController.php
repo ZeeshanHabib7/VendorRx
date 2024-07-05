ff4110f4d726ce605106a8a188a5414bc3a8cb99
hfatima08 <fatimaah812@gmail.com>
2024-07-05 15:04:28 +0500

    added pagination in product fetch api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -25,14 +25,52 @@ class ProductController extends Controller
             })
             ->when($request->filled('keyword'), function ($query) use ($request) {
                 return $query->where('name', 'like', '%' . $request->keyword . '%');
-            })
-            ->get();
+            });
+            
     
-            return ResponseHelper::success(new ProductCollection($products), 'Data fetched successfully!');
+            
+            if($request->input('paginate') == 'true'){
+                $paginatedData = $this->paginate($products->paginate());
+                return ResponseHelper::success($paginatedData, 'Data fetched successfully!');
+            } else {
+                return ResponseHelper::success(new ProductCollection($products->get()), 'Data fetched successfully!');
+            }
+
         }
         catch (Exception $e) {
             return ResponseHelper::error($e->getMessage(), $e->getCode());
         }
       
     }
+
+    public function paginate($data = []) {
+        $paginationArray = null;
+
+        if ($data != null) {
+            // Initialize pagination array with list of items
+            $paginationArray = [
+                'list' => $data->items(),
+                'pagination' => []
+            ];
+
+            // Add pagination details
+            $paginationArray['pagination'] = [
+                'total' => $data->total(),
+                'current' => $data->currentPage(),
+                'first' => 1,
+                'last' => $data->lastPage(),
+                'previous
+                ' => $data->currentPage() > 1 ? $data->currentPage() - 1 : 0,
+                'next' => $data->hasMorePages() ? $data->currentPage() + 1 : $data->lastPage(),
+                'pages' => $data->lastPage() > 1 ? range(1, $data->lastPage()) : [1],
+                'from' => $data->firstItem(),
+                'to' => $data->lastItem()
+            ];
+
+            return $this->result = $paginationArray;
+        }
+
+        return $paginationArray;
+    }
+
 }
