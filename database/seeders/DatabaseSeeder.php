be0e208b5bbb7abb9d08e6abae7fe6eaa53ac05f
Syed Samroze Ali <samroze3@gmail.com>
2024-07-08 15:41:55 -0700

    Commented the function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(ProductsSeeder::class);
+        //$this->call(ProductsSeeder::class);
     }
 }
