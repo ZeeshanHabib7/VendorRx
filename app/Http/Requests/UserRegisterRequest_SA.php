ec3fb7183f5f26e103db6c8ac4926d7aa7b1aa80
Zeeshan Habib <zeeshanhabib21@gmail.com>
2024-07-09 04:14:04 +0500

    fix validation file

--- app/Http/Requests/UserRegisterRequest_SA.php
+++ app/Http/Requests/UserRegisterRequest_SA.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserRegisterRequest_SA extends FormRequest
 {
-    /**
+        /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
@@ -21,15 +23,28 @@ class UserRegisterRequest_SA extends FormRequest
      */
     public function rules(): array
     {
-
-        $validate = [
+        return [
             'name' => 'required|string|min:2|max:255',
             'email' => 'required|string|max:100|email|unique:users',
-            'password' => 'required|string|min:8|confirmed'
+            'password' => 'required|string|min:8'
         ];
+    }
 
-        dd($validate);
-
-        return $validate;
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'status_code' => 422,
+            'message' => $validator->errors(),
+            'data'    => []
+        ], 422));
     }
 }
