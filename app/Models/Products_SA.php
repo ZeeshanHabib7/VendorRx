eebb2aff816acef5b24c775d28b6b62b8e3f4e42
Syed Samroze Ali <samroze3@gmail.com>
2024-07-09 11:42:39 -0700

    Query filter function added to enhance readiblity

--- app/Models/Products_SA.php
+++ app/Models/Products_SA.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Requests\ProductRequest_SA;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,48 @@ class Products_SA extends Model
     use HasFactory;
     protected $table = "products";
     protected $primaryKey = "id";
+
+    static public function getFilteredProducts(ProductRequest_SA $request)
+    {
+        $query = Products_SA::query();
+
+        // Apply filters
+        if ($request->has('name')) {
+            $query->where('name', 'LIKE', '%' . $request->input('name') . '%');
+        }
+
+        if ($request->has('startDate') && $request->has('endDate')) {
+
+            $query->whereBetween('created_at', [$request->input('startDate'), $request->input('endDate')]);
+
+        } else {
+
+            if ($request->has('startDate')) {
+
+                $query->where('created_at', '>=', $request->input('startDate'));
+
+            } elseif ($request->has('endDate')) {
+
+                $query->where('created_at', '<=', $request->input('endDate'));
+            }
+        }
+
+        if ($request->has('minPrice')) {
+            $query->where('price', '>=', $request->input('minPrice'));
+        }
+
+        if ($request->has('maxPrice')) {
+            $query->where('price', '<=', $request->input('maxPrice'));
+        }
+
+        if ($request->has('brand')) {
+            $query->where('brand', $request->input('brand'));
+        }
+
+        if ($request->has('keyWord')) {
+            $query->where('brand', 'LIKE', '%' . $request->keyWord . '%')->orWhere('name', 'LIKE', '%' . $request->keyWord . '%');
+        }
+
+        return $query;
+    }
 }
