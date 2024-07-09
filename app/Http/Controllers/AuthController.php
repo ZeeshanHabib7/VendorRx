d6548c5ec74d9aa3ecdeeb6d17af590d89ca10c3
hfatima08 <fatimaah812@gmail.com>
2024-07-09 15:38:22 +0500

    added signup functionality in auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function signUp(UserRequest $request){
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = auth('api')->login($user);
+        return $this->respondWithToken($token);
+
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
  
 }
