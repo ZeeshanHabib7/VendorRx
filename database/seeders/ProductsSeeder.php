ce7563f9eb0a6387450a872c5857e6cc9160da30
Syed Samroze Ali <samroze3@gmail.com>
2024-07-09 05:36:57 -0700

    Corrected syntax errors

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Products;
+use App\Models\Products_SA;
 use Faker\Factory as Faker;
 
 class ProductsSeeder extends Seeder
@@ -19,7 +19,7 @@ class ProductsSeeder extends Seeder
 
         for ($i = 1; $i < 50; $i++) {
 
-            $product = new Products;
+            $product = new Products_SA;
             $product->name = $faker->word;
             $product->price = $faker->randomFloat(2, 100, 2000);
             $product->brand = $faker->company;
