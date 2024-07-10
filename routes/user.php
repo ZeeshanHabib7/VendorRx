a351df36629bb8ec7397eae600c949a2cf7188d8
hfatima08 <fatimaah812@gmail.com>
2024-07-11 01:26:51 +0500

    protected products route

--- routes/user.php
+++ routes/user.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
 //-------- Auth Routes --------
@@ -14,6 +15,8 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('me', [AuthController::class,'me']); //user route
     Route::post('logout', [AuthController::class,'logout']);
     Route::post('refresh', [AuthController::class,'refresh']); //token refresh route
+    
+    Route::get('products',[ProductController::class,'index']);     
 });
 
 Route::get('all', [UserController::class, 'getAllUsers']);
