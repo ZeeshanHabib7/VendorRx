6f82ddb36b05afd7eb124c2c478f504a20dc8d44
Syed Samroze Ali <samroze3@gmail.com>
2024-07-10 15:49:07 -0700

    Login request modified for feature testing

--- app/Http/Requests/UserLoginRequest_SA.php
+++ app/Http/Requests/UserLoginRequest_SA.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserLoginRequest_SA extends FormRequest
 {
@@ -22,8 +24,31 @@ class UserLoginRequest_SA extends FormRequest
     public function rules(): array
     {
         return [
+            'password' => 'required|string',
             'email' => 'required|string|email|exists:users,email',
-            'password' => 'required|string'
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Email is required.',
+            'email.string' => 'Enter a email in string.',
+            'email.email' => 'Enter a valid email.',
+            'email.exists' => 'The user does not exist. Please Register yourself.',
+            'password.required' => 'Password field is required for the registration.',
+            'password.min' => 'Password must me minimum of 8 characters long.'
+        ];
+    }
+
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'status_code' => 422,
+            'message' => $validator->errors(),
+            'data' => []
+        ], 422));
     }
 }
