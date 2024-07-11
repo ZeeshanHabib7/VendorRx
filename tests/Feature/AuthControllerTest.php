40b6ee7fe9ecd8b264a17c48c95ffc265a670ab6
hfatima08 <fatimaah812@gmail.com>
2024-07-11 16:21:03 +0500

    added login tests

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -3,12 +3,13 @@
 namespace Tests\Feature;
 
 use App\Http\Requests\UserSignUpRequest;
+use App\Http\Requests\UserLoginRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
-    //  use RefreshDatabase;
+     // use RefreshDatabase;
 
     public function testSignUpWithValidData(): void
     {
@@ -42,6 +43,42 @@ class AuthControllerTest extends TestCase
 
         // Call the controller method
         $response = $this->post('/user/signup', $request->all());
+
+        // Assert response status and structure
+        $response->assertStatus(422)
+        ->assertJsonStructure([
+            "success", "status_code", "message", "data"
+        ]);
+    }
+
+    public function testLoginWithValidData(): void
+    {
+        // Simulate a request with necessary parameters
+        $request = new UserLoginRequest([
+            'email' => 'testuser@gmail.com',
+            'password' => 'test1234',
+        ]);
+
+        // Call the controller method
+        $response = $this->post('/user/login', $request->all());
+
+        // Assert response status and structure
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            "success", "status_code", "message", "data"
+        ]);
+    }
+
+    public function testLoginWithInValidData(): void
+    {
+        // Simulate a request with necessary parameters
+        $request = new UserLoginRequest([
+            'email' => 'test',
+            'password' => ' ',
+        ]);
+
+        // Call the controller method
+        $response = $this->post('/user/login', $request->all());
 
         // Assert response status and structure
         $response->assertStatus(422)
