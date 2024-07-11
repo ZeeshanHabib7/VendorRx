85f26d420b5d5331524ba80f24deeb27d563e18d
hfatima08 <fatimaah812@gmail.com>
2024-07-12 00:58:02 +0500

    removed constructor from user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -7,18 +7,10 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    // private $token;
-    // private $user;
-
-    // public function __construct($resource)
-    // {
-    //     $this->token = $resource['token'];
-    //     $this->user = $resource['user'];
-    // }
-
     public function toArray(Request $request): array
     {
         // Return reponse if token exists 
+        
         if(!empty($this->resource['token'])){
             return [
                 'token' => $this->resource['token'],
