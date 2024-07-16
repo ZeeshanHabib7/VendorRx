573eab140bcab3aad9905d2f6d6c0e608ad6489a
Syed Samroze Ali <samroze3@gmail.com>
2024-07-16 16:45:33 +0500

    Middleware for permissions added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'AuthGuard' => \App\Http\Middleware\ProductGuard::class,
+        'AdminCheck'=>\App\Http\Middleware\RoleMiddleware::class,
+        'PermissionCheck'=> \App\Http\Middleware\PermissionMiddleware::class,
     ];
 }
