21548f7edb1909b6bd74a54fb75027d9eac281e6
hfatima08 <fatimaah812@gmail.com>
2024-07-18 23:26:44 +0500

    updated product request file

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ProductRequest extends FormRequest
 {
@@ -42,5 +44,15 @@ class ProductRequest extends FormRequest
 
             'date.date' => 'The date field must be a valid date',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'status_code' => 422,
+            'message' => $validator->errors(),
+            'data'    => []
+        ], 422));
     }
 }
