f41038a41dd8428db3796ebf917f4076ed6a8054
Syed Samroze Ali <samroze3@gmail.com>
2024-07-25 04:35:46 +0500

    Response helper modified for general use

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -9,21 +9,21 @@ use App\Http\Resources\ProductResource;
 class ResponseHelper
 {
 
-    public static function sendResponse($sucess, $code, $msg, $products = [], $paginate = false, $pageSize = 5, $pageNo = 1)
+    public static function sendResponse($sucess, $code, $msg, $data = [], $paginate = false, $pageSize = 5, $pageNo = 1)
     {
 
         $response = [
             'success' => $sucess,
             'status_code' => $code,
             'message' => [$msg],
-            'data' => ProductResource::collection($products),
+            'data' => $data,
 
 
         ];
 
         if ($paginate) {
-            $products = self::paginate($products, $pageNo, $pageSize);
-            $response['data'] = $products;
+            $data = self::paginate($data, $pageNo, $pageSize);
+            $response['data'] = $data;
         }
 
         return $response;
@@ -37,7 +37,7 @@ class ResponseHelper
             // Initialize pagination array with list of items
             //dd($data);
             $paginationArray = [
-                'list' => ProductResource::collection($data->items()),
+                'list' => $data,
                 'pagination' => []
             ];
 
