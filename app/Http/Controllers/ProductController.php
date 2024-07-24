2d5a6b1d5aba3b0d57f42612749a4db248f75e98
Syed Samroze Ali <samroze3@gmail.com>
2024-07-24 17:23:01 +0500

    Naming conventions of classes corrected from Product --> Products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductCollection;
 use App\Http\Interfaces\CrudInterface_FH;
-use App\Models\Product;
+use App\Models\Products;
 use Exception;
 
 class ProductController extends Controller implements CrudInterface_FH
@@ -23,14 +23,14 @@ class ProductController extends Controller implements CrudInterface_FH
             if ($request->query()) {
                 // calling function to filter products based on request params 
                 $filteredProducts = $this->filterProduct($request);
-                
+
                 // check for pagination
-                if ($request->input('paginate')){
+                if ($request->input('paginate')) {
                     $pageSize = $request->input('pageSize', $this->defaultPageSize);
                     $pageNum = $request->input('pageNum', $this->defaultPageNum);
                     $this->isPaginate = true;
                     $products = $filteredProducts->paginate($pageSize, ['*'], 'page', $pageNum);
-                } 
+                }
                 // else fetch all products without pagination
                 else {
                     $products = $filteredProducts->get();
@@ -38,36 +38,36 @@ class ProductController extends Controller implements CrudInterface_FH
             }
             // else fetch all products
             else {
-                 $products = $this->index();
+                $products = $this->index();
             }
-        
-            return successResponse( 'Data fetched successfully!',ProductCollection::collection($products),$this->isPaginate, 200);
-        }
-        catch (Exception $e) {
+
+            return successResponse('Data fetched successfully!', ProductCollection::collection($products), $this->isPaginate, 200);
+        } catch (Exception $e) {
             return handleException($e);
-        }   
+        }
     }
 
     // Filtering products with respect to request params
-    public function filterProduct($request) {
-       return Product::when($request->filled('startDate') && $request->filled('endDate'), function ($query) use ($request) {
+    public function filterProduct($request)
+    {
+        return Products::when($request->filled('startDate') && $request->filled('endDate'), function ($query) use ($request) {
             return $query->whereBetween('date', [$request->startDate, $request->endDate]);
         })
-        ->when($request->start_date, function ($query, $startDate) {
-            return $query->where('date', '>=', $startDate);
-        })
-        ->when($request->end_date, function ($query, $endDate) {
-            return $query->where('date', '<=', $endDate);
-        })
-        ->when($request->filled('brand'), function ($query) use ($request) {
-            return $query->where('brand', $request->brand);
-        })
-        ->when($request->filled('minPrice') && $request->filled('maxPrice'), function ($query) use ($request) {
-            return $query->whereBetween('price', [$request->minPrice, $request->maxPrice]);
-        })
-        ->when($request->filled('keyword'), function ($query) use ($request) {
-            return $query->where('name', 'like', '%' . $request->keyword . '%');
-        });   
+            ->when($request->start_date, function ($query, $startDate) {
+                return $query->where('date', '>=', $startDate);
+            })
+            ->when($request->end_date, function ($query, $endDate) {
+                return $query->where('date', '<=', $endDate);
+            })
+            ->when($request->filled('brand'), function ($query) use ($request) {
+                return $query->where('brand', $request->brand);
+            })
+            ->when($request->filled('minPrice') && $request->filled('maxPrice'), function ($query) use ($request) {
+                return $query->whereBetween('price', [$request->minPrice, $request->maxPrice]);
+            })
+            ->when($request->filled('keyword'), function ($query) use ($request) {
+                return $query->where('name', 'like', '%' . $request->keyword . '%');
+            });
     }
 
     // get all products
@@ -75,30 +75,28 @@ class ProductController extends Controller implements CrudInterface_FH
     {
         try {
             // get all products
-            return Product::all();
-        } 
-        catch (\Exception $e) {
+            return Products::all();
+        } catch (Exception $e) {
             // Handle any exceptions that may occur during the process
             return handleException($e);
         }
     }
 
     // Add product
-    public function create(ProductRequest $request) 
+    public function create(ProductRequest $request)
     {
         $validatedData = $request->validated();
         return $this->store($validatedData);
     }
 
     public function store(array $payload)
-    { 
+    {
         try {
             // create product
-            $product = Product::create($payload);
+            $product = Products::create($payload);
             // success reponse upon creation
             return successResponse("Product Added Successfully!", ProductCollection::make($product));
-        } 
-        catch (\Exception $e) {
+        } catch (Exception $e) {
             // Handle any exceptions that may occur during the process
             return handleException($e);
         }
@@ -107,37 +105,36 @@ class ProductController extends Controller implements CrudInterface_FH
     public function edit(ProductRequest $request, $id)
     {
         $validatedData = $request->validated();
-        return $this->update($validatedData,$id);
+        return $this->update($validatedData, $id);
     }
 
 
     public function update(array $payload, $id)
     {
-        try{
-            $product = Product::findOrFail($id);
+        try {
+            $product = Products::findOrFail($id);
             // update product
-            $product->update($payload);  
+            $product->update($payload);
             // get updated product
-            $updatedproduct = Product::find($product->id);  
+            $updatedproduct = Products::find($product->id);
             // success response upon updation
-            return successResponse("Product Updated Successfully!",ProductCollection::make($updatedproduct));
-        } 
-        catch (\Exception $e) {
+            return successResponse("Product Updated Successfully!", ProductCollection::make($updatedproduct));
+        } catch (Exception $e) {
             // Handle any exceptions that may occur during the process
             return handleException($e);
-        }    
-        
+        }
+
     }
 
     public function destroy($id)
     {
         try {
             // delete product
-            $product = Product::find($id);
+            $product = Products::find($id);
             $product->delete();
             // success response upon deletion
             return successResponse("Product Deleted Successfully!");
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             // Handle any exceptions that may occur during the process
             return handleException($e);
         }
