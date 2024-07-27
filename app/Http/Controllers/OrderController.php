41703f20f5ae32821b76d4f5e26a5d2ab921f9ba
Syed Samroze Ali <samroze3@gmail.com>
2024-07-27 16:33:56 +0500

    Redundant code removed

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\ResponseHelper;
+
 use App\Http\Requests\OrderRequest;
 use App\Http\Resources\Order_Resource;
 use App\Models\Order;
-use Illuminate\Http\Request;
+
 
 class OrderController extends Controller
 {
@@ -14,34 +14,42 @@ class OrderController extends Controller
     {
 
         try {
-            $userId = $request->user()->id;
 
-            //If there are no query parameters we will return the whole data
+            $userId = null;
+
+            //If data is required by the 'User' role we will save his id
+            if ($request->user()->hasRole('user'))
+                $userId = getCurrentUserId();
+
+            // If no query parameters    
             if (!$request->query()) {
-                $orders = Order::where('user_id', $userId)->orderBy('created_at', 'desc')->get();
+
+                //Means we have to send all the data (If this is 'Admin' userId would be null and we will get all users data and if it is a 'User' than we will get only that user's data without any filters)
+                $orders = Order::getOrders($userId)->get();
 
             } else {
 
-                //Now we will get the filtered data acc to our query parameters
-                $query = Order::getFilterOrders($request, $userId);
+                // If we have any query param, now we will get the filtered data acc to our query parameters
+                $orders = Order::getFilterOrders($request, $userId);
 
-                // If we have data in our query and we also have pagination
-                if ($query->exists() && $request->paginate) {
-
-                    $orders = $query->paginate($request->pageSize, ['*'], 'page', $request->pageNo);
-
-                    // if only data is retrieved and no pagination
-                } else if ($query->exists()) {
-
-                    $orders = $query->get();
-
-                } else {
+                if (!$orders->exists()) {
                     // sending error response means we didn't find data for given filters 
                     return errorResponse("No orders fetched", 404);
-                }
-            }
 
-            return response()->json(ResponseHelper::sendResponse(true, 200, "Data Fetched Successfully", Order_Resource::collection($orders), $request->paginate, $request->pageSize, $request->pageNo));
+                } else if ($request->paginate == true) {
+                    // If we have data in our query and we also have pagination
+                    $orders = $orders->paginate($request->pageSize);
+
+                } else {
+                    $orders = $orders->get();
+                }
+
+
+
+            }
+            return successResponse("Orders fetched successfully", Order_Resource::collection($orders), $request->paginate, 200);
+
+
 
         } catch (\Exception $e) {
 
