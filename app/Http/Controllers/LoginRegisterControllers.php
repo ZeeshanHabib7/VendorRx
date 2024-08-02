9214c6f895525d80c73cc36a650e632c6cda5512
Syed Samroze Ali <samroze3@gmail.com>
2024-08-03 03:22:48 +0500

    Functions for Forget and Reset passwords added

--- app/Http/Controllers/LoginRegisterControllers.php
+++ app/Http/Controllers/LoginRegisterControllers.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ForgetPasswordRequest;
+use App\Http\Requests\ResetPasswordRequest;
 use App\Http\Requests\UserRegisterRequest_SA;
 use App\Http\Requests\UserLoginRequest_SA;
+use App\Mail\ForgetPasswordMail;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Resources\UserResource_SA;
@@ -11,6 +14,8 @@ use App\Http\Resources\UserResource;
 
 
 use Auth;
+use Mail;
+use URL;
 
 class LoginRegisterControllers extends Controller
 {
@@ -54,4 +59,65 @@ class LoginRegisterControllers extends Controller
         return Auth::guard('api')->attempt($request->only('email', 'password'));
     }
 
+    public function forgetPassword()
+    {
+
+        return view("forget-password");
+    }
+
+    public function sendEmail(ForgetPasswordRequest $request)
+    {
+        try {
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!is_null($user)) {
+                $domain = URL::to('/');
+                $url = $domain . '/users/reset-password/' . $user->id;
+                $data = [
+                    "url" => $url,
+                    "email" => $user->email,
+                    "title" => "Reset Password Link",
+                    "body" => "Please click on the below button to reset your password",
+                ];
+
+                Mail::to('k224187@nu.edu.pk')->send(new ForgetPasswordMail($data));
+
+                return back()->with('success', 'Email sent successfully. Please check your inbox');
+            }
+
+        } catch (\Exception $e) {
+            return back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+    public function resetPasswordPageLoad($id)
+    {
+        try {
+
+            $user = User::where('id', $id)->first();
+            if (!is_null($user))
+                return view("reset-password")->with('email', $user->email);
+
+        } catch (\Exception $e) {
+            return errorResponse($e->getMessage());
+        }
+
+    }
+
+    public function resetPassword(ResetPasswordRequest $request)
+    {
+        try {
+
+            $user = User::where('email', $request->email)->first();
+
+            $user->password = $request->password;
+            $user->save();
+
+            return back()->with('success', "Password changed successfully");
+
+        } catch (\Exception $e) {
+            return back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
 }
