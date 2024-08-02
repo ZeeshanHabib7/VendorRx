ea6f7cc12343ae80d3deb02eef31b8459dcf866c
Syed Samroze Ali <samroze3@gmail.com>
2024-08-03 03:24:02 +0500

    Mail class added for Forgot Password

---
+++ app/Mail/ForgetPasswordMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ForgetPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $mailData;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->mailData = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Forget Password Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'Mails.forget-password-mail',
+            // with: [
+            //     "url" => $data["url"],
+            //     "email" => $data,
+            //     "title" => "Reset Password Link",
+            //     "body" => "Please click on the below button to reset your password",
+            // ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
