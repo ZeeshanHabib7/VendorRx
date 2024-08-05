690024f950dc6cb7e1297f4354517975d7d35444
hfatima08 <fatimaah812@gmail.com>
2024-08-05 17:52:49 +0500

    changed use resource file used in user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Http\Resources\UserResource;
-use Illuminate\Http\Request;
-use App\Http\Resources\UserResource_SA;
 use App\Http\Requests\UserSearchRequest;
 
 class UserController extends Controller
@@ -25,13 +23,13 @@ class UserController extends Controller
                 $this->paginate = true;
 
                 // Perform pagination and format result as a resource collection
-                $result = UserResource_SA::collection($users->paginate($noOfRecordPerPage));
+                $result = UserResource::collection($users->paginate($noOfRecordPerPage));
             } elseif (isset($input['id']) && !empty($input['id'])) {
                 // Retrieve a specific user by ID and format result as a single resource
-                $result = UserResource_SA::make($users->findOrFail($input['id']));
+                $result = UserResource::make($users->findOrFail($input['id']));
             } else {
                 // Retrieve all users and format result as a resource collection
-                $result = UserResource_SA::collection($users->get());
+                $result = UserResource::collection($users->get());
             }
 
             // Return success response with the formatted result
