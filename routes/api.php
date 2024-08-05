5a635802f65fe4068761cd9b6e2d203d2f2d53b0
HASNAINRAZA\hasna <hasnain.raza.2003@gmail.com>
2024-08-05 16:02:46 +0500

    brand and category routes

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductsController_SA;
 use App\Http\Controllers\LoginRegisterControllers;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
 
 
 
@@ -40,7 +42,7 @@ Route::post('/products/add-to-cart', [AddToCartController::class, 'store'])->mid
 
 //PROTECTED Routes ---> For Admins Only
 Route::middleware(['AuthGuard', 'AdminCheck'])->group(function () {
-
+    
     Route::post('/products', [ProductsController_SA::class, 'store'])->middleware('PermissionCheck:product.store');
     Route::get('/products/{id}', [ProductsController_SA::class, 'show'])->middleware('PermissionCheck:product.view');
     Route::put('/products/{id}', [ProductsController_SA::class, 'update'])->middleware('PermissionCheck:product.update');
@@ -57,6 +59,21 @@ Route::middleware(['AuthGuard', 'AdminCheck'])->group(function () {
 
 Route::get('all', [UserController::class, 'getAllUsers']);
 
+Route::prefix('brands')->group(function () {
+    Route::get('/', [BrandController::class, 'index'])->name('brands.index');
+    Route::post('/', [BrandController::class, 'store'])->name('brands.store');
+    Route::get('{id}', [BrandController::class, 'show'])->name('brands.show');
+    Route::put('{id}', [BrandController::class, 'update'])->name('brands.update');
+    Route::delete('{id}', [BrandController::class, 'destroy'])->name('brands.destroy');
+});
+
+Route::prefix('categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
+    Route::post('/', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('{id}', [CategoryController::class, 'show'])->name('categories.show');
+    Route::put('{id}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+});
 
 
 
