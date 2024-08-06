abc5298c3c63da85ba0a777ccb6dd1fc367690a6
Syed Samroze Ali <samroze3@gmail.com>
2024-08-07 04:08:33 +0500

    Validation errors updated

--- public/JS/reset-password.js
+++ public/JS/reset-password.js
@@ -5,11 +5,17 @@ document.getElementById('reset-password').addEventListener('submit', function (e
     const submitBtn = document.querySelector("form button");
     const formData = new FormData(this);
     console.log("Form Data: ");
-    // for (let [key, value] of formData.entries()) {
-    //     console.log(`${key}: ${value}`);
-    // }
+    for (let [key, value] of formData.entries()) {
+        console.log(`${key}: ${value}`);
+    }
     const successElement = document.getElementById('success');
     const errorsElement = document.getElementById('errors');
+    const passwordError = document.getElementById('password');
+    const confirmPasswordError = document.getElementById('confirm-password');
+    const passwordInput = document.querySelector(".password input");
+    const confirmpasswordInput = document.querySelector(".confirm-password input");
+    const confirmpasswordLabel = document.querySelector(".confirm-password label");
+    const passwordLabel = document.querySelector(".password label");
 
 
     // Send the form data using fetch
@@ -36,17 +42,40 @@ document.getElementById('reset-password').addEventListener('submit', function (e
             } else {
                 // If success is false we display error message
                 console.error('Error:', data);
-                errorsElement.innerText = data.message.password || data.message.email;
-                setTimeout(() => {
-                    errorsElement.innerText = "";
-                }, 3000)
+                if (data.message.password == "The password field confirmation does not match.") {
+                    confirmPasswordError.innerText = data.message.password;
+                    confirmpasswordInput.style.border = "1px solid red";
+                    confirmpasswordLabel.style.color = "red";
+                    setTimeout(() => {
+                        confirmPasswordError.innerText = "";
+                        confirmpasswordInput.style.border = "1px solid rgb(19, 92, 226)";
+                        confirmpasswordLabel.style.color = "rgb(19, 92, 226)";
+                    }, 5000)
+                }
+                else if (data.message.password) {
+                    passwordError.innerText = data.message.password;
+                    passwordInput.style.border = "1px solid red";
+                    passwordLabel.style.color = "red";
+                    setTimeout(() => {
+                        passwordError.innerText = "";
+                        passwordInput.style.border = "1px solid rgb(19, 92, 226)";
+                        passwordLabel.style.color = "rgb(19, 92, 226)";
+                    }, 5000)
+                }
+                else {
+                    errorsElement.innerText = data.message.password || data.message.email;
+                    setTimeout(() => {
+                        errorsElement.innerText = "";
+                    }, 5000)
+                }
+
 
             }
         }).catch(error => {
             errorsElement.innerText = "An error occured please try again";
             setTimeout(() => {
                 errorsElement.innerText = "";
-            }, 3000)
+            }, 5000)
 
         })
 
